9d0ef8115fe6d768e46c0c8a43932e179e4eee51
xzjch <61073313+xzjch@users.noreply.github.com>
2020-04-11 17:31:46 +0800

    Update GetIssues.java

--- src/sonar/GetIssues.java
+++ src/sonar/GetIssues.java
@@ -38,7 +38,7 @@ public class GetIssues {
 		String url = "http://39.106.124.113:9000/api/issues/search?componentKeys=ks-cms-unicorn&s=FILE_LINE&resolved=false&types=CODE_SMELL,BUG,VULNERABILITY&additionalFields=_all";
 		String str = doGet(url, cookie.toString());
 		System.out.println(str);
-		writeFile(str, "./issues.txt");
+		writeFile(str, "../issues.txt");
 		
 	}
 	
